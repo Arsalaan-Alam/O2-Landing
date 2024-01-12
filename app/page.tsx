56b2de684136a7d9f8b7b8f09fb1b095ce274ca6
Arsalaan-Alam <studyfreak1234@gmail.com>
2024-01-12 17:16:33 +0530

    init

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+import AboutSectionOne from "@/components/About/AboutSectionOne";
+import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+import ScrollUp from "@/components/Common/ScrollUp";
+import Features from "@/components/Features";
+import Hero from "@/components/Hero";
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Free Next.js Template for Startup and SaaS",
+  description: "This is Home for Startup Nextjs Template",
+  // other metadata
+};
+
+export default function Home() {
+  return (
+    <>
+      <ScrollUp />
+      <Hero />
+      <Features />
+      <AboutSectionOne />
+      <AboutSectionTwo />
+
+    </>
+  );
+}
---
+++ components/Header/menuData.tsx
@@ -0,0 +1,25 @@
+import { Menu } from "@/types/menu";
+
+const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/",
+    newTab: false,
+  },
+  {
+    id: 2,
+    title: "Our Gitcoin Round",
+    path: "https://tinyurl.com/Dapilink",
+    newTab: false,
+  },
+  
+  {
+    id: 3,
+    title: "Talk with Us",
+    path: "https://calendly.com/arsalaanalam/30min",
+    newTab: false,
+  },
+
+];
+export default menuData;
